b73b265629b526bce41c062e89707143ac8c76bc
Michel Blanc <mb@mbnet.fr>
2017-01-07 23:01:20 +0100

    Removes dead code
    Removed unused modules
    Removed commented code

--- filedrop.py
+++ filedrop.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads, ALL
 
-import os
-import sys
-
-#app_root = os.environ.get('APPLICATION_ROOT', '/')
-#print(" * Using root:  %s" % app_root, file=sys.stderr)
-
 fileset = UploadSet('files', ALL)
 
 app = Flask(__name__)
 app.config['UPLOADED_FILES_DEST'] = 'uploads/'
 
 configure_uploads(app, fileset)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
